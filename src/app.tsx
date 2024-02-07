bed0f92178b357f1d846a75cea28fd73a1982387
Pablo Alan <pabloxt14@gmail.com>
2024-02-07 00:04:27 -0300

    feat: using note components on the home

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,6 @@
 import logo from './assets/logo-nlw-expert.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note.card'
 
 export function App() {
   return (
@@ -16,50 +18,11 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid auto-rows-[250px] grid-cols-3 gap-6">
-        <div className="space-y-3 rounded-md bg-slate-700 p-5">
-          <span className="text-sm font-medium text-slate-200">
-            Adicionar nota
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Grave uma nota em áudio que será convertida para texto
-            automaticamente.
-          </p>
-        </div>
+        <NewNoteCard />
 
-        <div className="relative space-y-3 overflow-hidden rounded-md bg-slate-800 p-5">
-          <span className="text-sm font-medium text-slate-300">há 2 dias</span>
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sequi
-            possimus magni, quasi debitis placeat.
-          </p>
-
-          <div className="pointer-events-none absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0" />
-        </div>
-
-        <div className="relative space-y-3 overflow-hidden rounded-md bg-slate-800 p-5">
-          <span className="text-sm font-medium text-slate-300">há 4 dias</span>
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sequi
-            possimus magni, quasi debitis placeat.
-          </p>
-
-          <div className="pointer-events-none absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0" />
-        </div>
-
-        <div className="relative space-y-3 overflow-hidden rounded-md bg-slate-800 p-5">
-          <span className="text-sm font-medium text-slate-300">há 7 dias</span>
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sequi
-            possimus magni, quasi debitis placeat. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Eum sequi possimus magni, quasi
-            debitis placeat. Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Eum sequi possimus magni, quasi debitis placeat. Lorem ipsum
-            dolor sit amet consectetur adipisicing elit. Eum sequi possimus
-            magni, quasi debitis placeat.
-          </p>
-
-          <div className="pointer-events-none absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0" />
-        </div>
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
       </div>
     </div>
   )
